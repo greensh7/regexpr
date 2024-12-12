3878d92c6f205cedb5d0d6595d1b88a88495b6ac
Shaun <greense69@gmail.com>
2024-12-11 19:57:21 -0600

    Updates for Qt6, python module, JS

--- src/pregexpr/pregexpr.py
+++ src/pregexpr/pregexpr.py
@@ -2,42 +2,45 @@
 
 """
 pregexpr.py
-Regex replacer 1.0
+Regex replacer 1.1
 
-Uncopyright (u)2019-2024, Shaun Green
+Uncopyright (u)2019-2025, Shaun Green
 
 """
 
 import sys
-import re
+#import re
+# To use the better regex module, on Ubuntu install python3-regex
+# The key features are noted on rexegg.com.
+import regex
 
 def main():
 	argc = len(sys.argv)
 	#print(str(sys.argv))
 	if argc<3 or argc>5:
-		print('PRegex Replacer 1.0')
+		print('PRegex Replacer 1.1')
 		print('Replaces text using a regular expression pattern and replace text (optional)')
 		print('')
 		print('Usage: ./pregexpr.py \"regexp\" \"text\" [replace text] [-m]')
 		return
 
-	regex = re.compile(sys.argv[1])
+	re = regex.compile(sys.argv[1])
 	text = sys.argv[2]
 	replace = ''
 	if argc>=4:
 		replace = sys.argv[3]
-	#print(regex.pattern, text, replace)
+	#print(re.pattern, text, replace)
 
 	if argc>=5 and sys.argv[4]=='-m':
 		replace = '-m'
 		#print(replace)
 
 	if replace == '-m':
-		match = regex.search(text)
+		match = re.search(text)
 		if match:
 			print(match.group())
 	else:
-		print(regex.sub(replace, text))
+		print(re.sub(replace, text))
 
 	return
 
--- tests/HelloWorld.sh
+++ tests/HelloWorld.sh
@@ -8,9 +8,9 @@ m=`grep -Pom1 "$rx" <<< "$text"`
 echo "BASH grep match:    ${m//$'\n'/ }"
 echo
 echo "Perl:              "`perl -pe "s/$rx/-/" <<< "$text"`
-echo "Qt 5.15.14:        "`../qregexpr "$rx" "$text" '-'`
+echo "Qt 6.8.1:          "`../qregexpr "$rx" "$text" '-'`
 echo "Go 1.21.6 regexp2: "`../gregexpr2 "$rx" "$text" '-'`
-echo "Python:            "`../pregexpr.py "$rx" "$text" '-'`
+echo "Python regex:      "`../pregexpr.py "$rx" "$text" '-'`
 echo "Javascript:        "`node ../jsregexpr.js "$rx" "$text" '-'`
 echo "Java:              "`( cd ../src/jregexpr; java jregexpr "$rx" "$text" '-')`
 echo
@@ -23,9 +23,9 @@ m=`grep -Pom1 "$rx" <<< "$text"`
 echo "BASH grep match:    ${m//$'\n'/ }"
 echo
 echo "Perl:              "`perl -pe "s/$rx//g" <<< "$text"`
-echo "Qt 5.15.14:        "`../qregexpr "$rx" "$text"`
+echo "Qt 6.8.1:          "`../qregexpr "$rx" "$text"`
 echo "Go 1.21.6 regexp2: "`../gregexpr2 "$rx" "$text"`
-echo "Python:            "`../pregexpr.py "$rx" "$text"`
+echo "Python regex:      "`../pregexpr.py "$rx" "$text"`
 echo "Javascript:        "`node ../jsregexpr.js "$rx" "$text"`
 echo "Java:              "`( cd ../src/jregexpr; java jregexpr "$rx" "$text" )`
 echo
@@ -37,9 +37,9 @@ m=`grep -Pom1 "$rx" <<< "$text"`
 echo "BASH grep match:    ${m//$'\n'/ }"
 echo
 echo "Perl:              "`perl -pe "s/$rx//g" <<< "$text"`
-echo "Qt 5.15.14:        "`../qregexpr "$rx" "$text"`
+echo "Qt 6.8.1:          "`../qregexpr "$rx" "$text"`
 echo "Go 1.21.6 regexp2: "`../gregexpr2 "$rx" "$text"`
-echo "Python:            "`../pregexpr.py "$rx" "$text"`
+echo "Python regex:      "`../pregexpr.py "$rx" "$text"`
 echo "Javascript:        "`node ../jsregexpr.js "$rx" "$text"`
 echo "Java:              "`( cd ../src/jregexpr; java jregexpr "$rx" "$text" )`
 echo
